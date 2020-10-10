43c88b4dd4bddcbed18d9fae8d95a345e3418e0b
Roberto Guineo A <rguineoa@gmail.com>
2020-10-09 21:22:11 -0300

    actualizacion del proyecto, creacion de un card

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ require_once "models/enrutamiento.model.php";
 	<title>Mi primer App</title>
 
 <!-- Librerias de estilos -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="views/dist/css/bootstrap.min.css">
 
 <link rel="stylesheet" type="text/css" href="views/dist/css/style.css">
 
@@ -29,9 +30,14 @@ require_once "models/enrutamiento.model.php";
 
 ?>
 
+
 <!-- librerias Javascript -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+<script type="text/javascript" href="views/dist/js/jquery-3.5.1.min.js"></script>
+
+<script type="text/javascript" href="views/dist/js/bootstrap.min.js"></script>
+
+
+
 </body>
 </html>
--- views/modulos/productos.php
+++ views/modulos/productos.php
@@ -1,1 +1,35 @@
-<h1>PRODUCTOS</h1>
+
+<div class="container">
+	
+	<div class="row">
+		
+		<div class="card" style="width: 18rem;">
+
+		  <img src="views/dist/img/tablets.jpg" class="card-img-top" alt="...">
+		  
+		  <div class="card-body">
+		    <h5 class="card-title">Tablets</h5>
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+		    <a href="#" class="btn btn-primary">ver mas</a>
+
+		  </div>
+		</div>
+
+
+		<div class="card" style="width: 18rem;">
+
+		  <img src="views/dist/img/pc-gamer.jpg" class="card-img-top" alt="...">
+		  
+		  <div class="card-body">
+		    <h5 class="card-title">PC Gamer</h5>
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+		    <a href="#" class="btn btn-primary">ver mas</a>
+
+		  </div>
+		</div>
+
+	</div>
+
+</div>
