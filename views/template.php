82bdd0202057fb05574303f49848a46202a49178
Roberto Guineo A <rguineoa@gmail.com>
2020-10-15 12:48:53 -0300

    actualizacion webapp

--- views/template.php
+++ views/template.php
@@ -9,8 +9,8 @@
 		if (isset($_SESSION["autenticar"]) &&  $_SESSION["autenticar"] == "ok") {
 			include_once "views/modulos/nav_bar.php";
 			echo "<hr>";
-			$contenido = new ctrErutamiento;
-			$contenido -> EnrutamientoController();		
+			$contenido = (new ctrErutamiento);
+			$respuesta = $contenido -> EnrutamientoController();		
 		} else {
 			include_once "views/modulos/login.php";
 		}
